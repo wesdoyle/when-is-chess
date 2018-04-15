7d304d935df5904c7f63b787fb3eef168493e9cb
wesdoyle <wesdoyle08@gmail.com>
2018-04-15 16:47:35 -0500

    Start project

---
+++ mpl/mpl/spiders/calendar_spider.py
@@ -0,0 +1,34 @@
+"""
+Finds chess events on the library calendar for and prints the date and location
+"""
+import scrapy
+
+BRANCHES = {'#ff0000': 'central',
+            '#ffa500': 'alicia_goodman',
+            '#008000': 'goodman_south',
+            '#00ced1': 'hawthorne',
+            '#0000ff': 'lakeview',
+            '#4b0082': 'meadowridge',
+            '#ee82ee': 'monroe_street',
+            '#708090': 'pinney',
+            '#a52a2a': 'sequoya'}
+
+
+def parse(response):
+    # todo: rather than this, iterate over each day div,
+    # store the date, if chess, add to list, else continue
+    for el in response.css('a.colorbox-inline::text').extract():
+        if el is not None:
+            if 'chess' in el.lower():
+                print(el)
+        else:
+            print('No chess events found.')
+
+
+class ChessCalendarSpider(scrapy.Spider):
+    name = "chess"
+
+    def make_request(self):
+        urls = ['https://madisonpubliclibrary.org/calendar/search']
+        for url in urls:
+            yield scrapy.Request(url=url, callback=parse)
