b84c00fc63046982980e037ded46f76283506af5
wesdoyle <wesdoyle08@gmail.com>
2018-04-15 21:04:58 -0500

    Create python script to spin up scrapy

---
+++ when-is-chess.py
@@ -0,0 +1,16 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from mpl.mpl.spiders.calendar_spider import ChessCalendarSpider
+
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+    'LOG_LEVEL': 'ERROR',
+    'ROBOTSTXT_OBEY': True,
+    'HTTPCACHE_ENABLED': True,
+    'HTTPCACHE_EXPIRATION_SECS': 86_400,  # 24 hours
+    'HTTPCACHE_DIR': 'httpcache',
+    'HTTPCACHE_STORAGE': 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+})
+
+process.crawl(ChessCalendarSpider)
+process.start()
